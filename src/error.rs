f9373e4bb7bde62cb9121252f2c4b0d14c27ce9b
MR-PROFESSOR-790 <231360@students.au.edu.pk>
2025-08-23 12:43:35 +0500

    Add EscrowError enum for error handling in escrow program

---
+++ src/error.rs
@@ -0,0 +1,20 @@
+use solana_program::program_error::ProgramError;
+use thiserror::Error;
+#[derive(Error, Debug,Copy, Clone)]
+pub enum EscrowError {
+     #[error("Invalid instruction")]
+    InvalidInstruction,
+    #[error("Unauthorized")]
+    Unauthorized,
+    #[error("Seller not confirmed")]
+    NotConfirmed,
+    #[error("Already confirmed")]
+    AlreadyConfirmed,
+}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
+
